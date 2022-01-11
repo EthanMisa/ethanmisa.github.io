1521036138196bfe6c561bad4cec6492a7413ade
Ethan Misa <ethan.misa@gmail.com>
2022-01-11 00:48:37 -0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,51 +2,7 @@ import React from "react";
 import "../styles/Footer.css";
 
 function getStringDate() {
-	const monthNames = [
-		"January",
-		"February",
-		"March",
-		"April",
-		"May",
-		"June",
-		"July",
-		"August",
-		"September",
-		"October",
-		"November",
-		"December",
-	];
-
-	const date = new Date();
-
-	var [month, day, year] = [
-		monthNames[date.getMonth()],
-		date.getDate(),
-		date.getFullYear(),
-	];
-
-	var ending;
-	if (day == 12 || day == 11 || day == 13) {
-		ending = "th ";
-	} else if (day % 10 == 1) {
-		ending = "st ";
-	} else if (day % 10 == 2) {
-		ending = "nd ";
-	} else if (day % 10 == 3) {
-		ending = "rd ";
-	} else {
-		ending = "th ";
-	}
-
-	return (
-		<em>
-			<>
-				Last modified on {month} {day}
-				<sup>{ending}</sup>
-			</>
-			<>{year}</>
-		</em>
-	);
+	return <em>Last modified on {document.lastModified}</em>;
 }
 
 function Footer() {
