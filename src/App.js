1504490c8be54c8efa6677694493e04f3988c484
Ethan Misa <ethan.misa@gmail.com>
2022-01-12 14:41:02 -0500

    update

--- src/App.js
+++ src/App.js
@@ -3,30 +3,17 @@ import Footer from "./components/Footer.js";
 import Sidebar from "./components/Sidebar.js";
 import Skills from "./components/Skills.js";
 import "./styles/App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Aidan from "./components/Aidan.js";
 
 function App() {
 	return (
-		// add helmet for title {process.env.PUBLIC_URL +
-		<BrowserRouter>
-			<Routes>
-				<Route
-					path="/"
-					element={
-						<div className="app">
-							<div className="content">
-								<About />
-								<Skills />
-								<Footer />
-							</div>
-							<Sidebar />
-						</div>
-					}
-				/>
-				<Route path="/aidan" element={<Aidan />} />
-			</Routes>
-		</BrowserRouter>
+		<div className="app">
+			<div className="content">
+				<About />
+				<Skills />
+				<Footer />
+			</div>
+			<Sidebar />
+		</div>
 	);
 }
 
