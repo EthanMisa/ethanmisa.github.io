244285157e30a133ea3f8066ad07c3b1c650e0b3
Ethan Misa <eamisa@uwaterloo.ca>
2023-11-06 19:27:44 -0500

    Cleaned up file structure and basic components

---
+++ src/components/centeredContainer/centeredContainer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+
+interface CenteredContainerProps {
+  children: React.ReactNode;
+}
+
+function CenteredContainer({ children }: CenteredContainerProps) {
+  return <div className="container">{children}</div>;
+}
+export default CenteredContainer;
